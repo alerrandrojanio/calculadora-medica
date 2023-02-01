c2f36eea460cd53e300d205b1d8051ddec31b89b
alerrandro <alerrandro.janio07@gmail.com>
2023-02-01 13:36:20 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,24 +5,26 @@ import logo from "../assets/img/logo.png"
 
 export function Header() {
   return (
-    <div>
+    <div className="w-full flex flex-col gap-4 px-4">
       <div>
         <img src={logo} className="w-[400px] m-auto p-4" alt="Logo LAC" />
       </div>
-      <div className="mt-8 font-semibold text-center text-xl flex justify-center gap-4 text-lac-blue-dark">
-        <Link
-          to={"/calculadora-medica/"}
-          className="text-xl font-semibold hover:text-lac-blue-medium"
-        >
-          Colesterol
-        </Link>{" "}
-        |
-        <Link
-          to={"/calculadora-medica/homa"}
-          className="text-xl font-semibold hover:text-lac-blue-medium"
-        >
-          Índice de Homa
-        </Link>
+      <div className="mt-6 mx-8 flex justify-center items-center">
+        <div className=" flex flex-wrap items-center justify-center gap-6 py-2 px-6 rounded-xl bg-lac-blue-dark">
+          <Link
+            to={"/calculadora-medica/"}
+            className="text-lg font-semibold text-white hover:text-lac-blue-light"
+          >
+            Colesterol LDL/VLDL
+          </Link>{" "}
+          <span className="text-white">|</span>
+          <Link
+            to={"/calculadora-medica/homa"}
+            className="text-lg font-semibold text-white hover:text-lac-blue-light"
+          >
+            Índice de Homa
+          </Link>
+        </div>
       </div>
     </div>
   )
